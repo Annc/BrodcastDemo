5c4617f1225fa11f1306fce9023a890e74356fec
DouDou <1411436751@qq.com>
2016-11-14 21:51:39 +0800

    注册broadcast

--- app/src/main/java/com/example/dou/brodcastdemo/MainActivity.java
+++ app/src/main/java/com/example/dou/brodcastdemo/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.dou.brodcastdemo;
 
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private MyReciver reciver = new MyReciver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /**
+         * 动态注册广播,需要注销
+         */
+        /*
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.intent.action.AIRPLANE_MODE");
+        registerReceiver(reciver, filter);
+        */
+
     }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //销毁广播
+        //unregisterReceiver(reciver);
+    }
+
+
 }
